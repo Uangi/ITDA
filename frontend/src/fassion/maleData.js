f2be5bfcceeba501e55b302e25578dd69996169d
Uangi <jyp423@naver.com>
2024-04-09 16:21:31 +0900

    git history test

---
+++ frontend/src/fassion/maleData.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        id: '1',
+        name : '스타일리시',
+        description:
+        ['가을', '겨울'],
+        photo : 'https://image.musinsa.com/images/style/list/2024040415535300000091956.jpg'
+    },
+    {
+        id: '2',
+        name : '가벼운 조합',
+        description:
+        ['봄', '여름'],
+        photo : 'https://image.musinsa.com/images/style/list/2024040415360400000088658.jpg'
+    },
+];

6932751c526ad7f448c7946a1ea772207f59bb4b
Uangi <jyp423@naver.com>
2024-04-10 15:53:10 +0900

    사용하지않는 파일 제거

--- frontend/src/fashion/maleData.js
+++ frontend/src/fashion/maleData.js
@@ -4,13 +4,19 @@ export default [
         name : '스타일리시',
         description:
         ['가을', '겨울'],
+        
+        weather:
+        'clear sky',
+
         photo : 'https://image.musinsa.com/images/style/list/2024040415535300000091956.jpg'
     },
     {
         id: '2',
         name : '가벼운 조합',
         description:
-        ['봄', '여름'],
+        ['가을', '겨울'],
+        weather:
+        'cloud',
         photo : 'https://image.musinsa.com/images/style/list/2024040415360400000088658.jpg'
     },
 ];

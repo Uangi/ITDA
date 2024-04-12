dce379e66d7484cdf1d0d208c6a60881b5527d82
Uangi <jyp423@naver.com>
2024-04-12 13:03:29 +0900

    크롤링 테스트

--- backend/src/main/java/com/itda/fashion/FashionService.java
+++ backend/src/main/java/com/itda/fashion/FashionService.java
@@ -1,5 +1,6 @@
 package com.itda.fashion;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,27 +12,36 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
 
 @Service
 public class FashionService {
-    private static String Fashion_URL = " https://www.hkbs.co.kr/news/articleList.html?sc_section_code=S1N1&view_type=sm";
+    private static String Fashion_URL = "크롤링할 데이터";
 
     @PostConstruct
     public List<Fashion> getFashionDatas() throws IOException {
         List<Fashion> fashionList = new ArrayList<>();
         Document document = Jsoup.connect(Fashion_URL).get();
 
-        Elements contents = document.select("section ul.type2 li");
+        // Elements contents = document.select("section ul.type2 li");
+
+        Elements contents = document.select("ul.style-list li.style-list-item");
+        // Elements contents2 = document.select("section ul.style-list
+        // style-list--full");
 
         for (Element content : contents) {
             Fashion fashion = Fashion.builder()
                     .image(content.select("a img").attr("abs:src")) // 이미지
-                    .subject(content.select("h4 a").text()) // 제목
-                    .url(content.select("a").attr("abs:href")) // 링크
+                    // .subject(content.select("h4 a").text())
+                    .subject(content.select("a span ").text()) // 설명
+
+                    // .url(content.select("a").attr("abs:href")) // 링크
                     .build();
             fashionList.add(fashion);
         }
 
         return fashionList;
     }
+
 }

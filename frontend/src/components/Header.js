9e2f51de16e8b543550bbbee855c6f8b91193c96
Uangi <jyp423@naver.com>
2024-04-29 14:13:23 +0900

    오리지널 헤더

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'; 
 import { Container, Nav, Navbar } from 'react-bootstrap'; 
 import { Link } from 'react-router-dom'; 
- 
+// import axios from 'axios';
+// import { IoDiamondOutline } from "react-icons/io5";
+
 const Header = () => { 
     return ( 
       <Navbar bg="dark" variant="dark"> 
@@ -10,7 +12,8 @@ const Header = () => {
           <Nav className="me-auto"> 
             <Link to ="/" className="nav-link">홈</Link> 
             <Link to ="/write" className="nav-link">글쓰기</Link>
-            <Link to ="/append" className="nav-link">날씨 조회</Link>
+            <Link to ="/append2" className="nav-link">날씨 조회</Link>
+            <Link to ="/Login" className="nav-link">로그인</Link>
           </Nav> 
         </Container> 
       </Navbar> 

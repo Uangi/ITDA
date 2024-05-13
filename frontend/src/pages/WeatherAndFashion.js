a0642e14cd30936b5c50007263a20cdd92e104f6
Uangi <jyp423@naver.com>
2024-05-13 09:32:46 +0900

    날씨 CSS  수정

--- frontend/src/pages/WeatherAndFashion.js
+++ frontend/src/pages/WeatherAndFashion.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import key from '../../apikey.js';
 import axios from 'axios';
+import address from '../../API_KEY';
+import './WeatherAndFashion.css';
+// import './Weather.css';
+import './Weather.css';
 
 const cityNamesMap = {
     '서울': 'Seoul',
@@ -21,7 +25,7 @@ const cityNamesMap = {
 const WeatherAndFashion = () => {
     const [currentTime, setCurrentTime] = useState('');
     const [weatherData, setWeatherData] = useState(null);
-    const [cityName, setCityName] = useState('Seoul');
+    const [cityName, setCityName] = useState('');
     const [fashionData, setFashionData] = useState([]);
 
     useEffect(() => {
@@ -29,6 +33,7 @@ const WeatherAndFashion = () => {
             const now = new Date();
             const month = now.getMonth() + 1;
             const date = now.getDate();
+            
             return `${month}월 ${date}일 `;
         };
 
@@ -38,7 +43,7 @@ const WeatherAndFashion = () => {
                 const cityNameInEnglish = cityNamesMap[cityName] || cityName;
                 const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityNameInEnglish},kr&appid=${WEATHER_API_KEY}`);
                 setWeatherData(weatherResponse.data);
-                await axios.get("http://localhost:4000/csvToDatabase", {withCredentials : true});
+                await axios.get(`${address.backendaddress}/csvToDatabase`, {withCredentials : true});
             } catch (error) {
                 console.error('Error fetching weather data:', error);
             }
@@ -67,7 +72,7 @@ const WeatherAndFashion = () => {
 
     const getFashionData = async () => {
         try {
-            const response = await axios.get("http://localhost:4000/api/fashion");
+            const response = await axios.get(`${address.backendaddress}/api/fashion`);
             const filteredFashionData = response.data.filter((item, index) => index !== 0);
             // Remove double quotes from image URLs
             const formattedFashionData = filteredFashionData.map(item => ({
@@ -82,37 +87,54 @@ const WeatherAndFashion = () => {
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={cityName} onChange={(e) => setCityName(e.target.value)} placeholder="지역명을 입력해주세요" />
-            </form>
-            <span className="nowtime">{currentTime}</span>
-            <span>현재날씨</span>
-            <h3>{cityName}</h3>
+        
+        <div style={{ margin: 'auto' }}>
+            <form onSubmit={handleSubmit} style={{ display: 'flex', justifyContent: 'center' }}>
+    <div>
+        <input type="text" value={cityName} className="weather_InputContainer" onChange={(e) => setCityName(e.target.value)} placeholder="지역명을 입력해주세요" />
+    </div>
+</form>
+
+            
             {weatherData && (
-                <div>
-                    <h3>{weatherData.weather[0].description}</h3>
-                    <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].description}/>
-                    <h3>현재기온: {weatherData.main.temp}</h3>
-                    <h3>최저기온: {weatherData.main.temp_min}</h3>
-                    <h3>최대기온: {weatherData.main.temp_max}</h3>
+                <div style={{textAlign: 'center'}}>
+                <div class="cmp-cur-weather wbg wbg-type2 BGDB00">
+                    <ul class="wrap-1">
+                        <li class="w-icon w-temp no-w">
+                            <span class="hid">{currentTime}{cityName}의 기온 </span><span class="wic DB00 large"></span>
+                        </li>
+		
+			
+		
+	</ul>
+	<ul class="wrap-2 no-underline">
+		<li><span class="lbl ic-hm">현재 기온<small>&nbsp;</small></span><span class="val">{weatherData.main.temp}℃</span></li>
+		
+		<li><span class="lbl ic-wind">최저 기온<small>&nbsp;</small></span><span class="val">{weatherData.main.temp_min}℃</span></li>
+		
+		<li><span class="lbl rn-hr1 ic-rn">최대 기온</span><span class="val">{weatherData.main.temp_max}℃</span></li>
+	</ul>
+</div>
+
                 </div>
             )}
-            <div>
-                <button onClick={handleFashionButtonClick}>추천 데이트룩 조회</button>
-            </div>
-            <div>
-                <h1>추천 패션 리스트</h1>
-                <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-                    {fashionData.map((item, index) => (
-                        <div key={index} style={{ marginRight: '20px' }}>
-                            <h4>{item.description}</h4>
-                            <img src={item.image} alt={item.description} width={150} />
-                        </div>
-                    ))}
+            <div style={{ width: '80%', margin: 'auto', marginTop: '50px', textAlign: 'center' }}>
+                <div>
+                    <button onClick={handleFashionButtonClick} className='datalook_button'>STYLING</button>
+                </div>
+                <div>
+                    <div style={{ display: 'flex',justifyContent:'center', flexDirection: 'row', flexWrap: 'wrap' }}>
+                        {fashionData.map((item, index) => (
+                            <div key={index} style={{ marginRight: '20px', marginBottom: index % 4 === 3 ? '20px' : '0', width: '23%' }}>
+                                <img src={item.image} alt={item.description} width={150} />
+                                <h6 className='desc'>{item.description}</h6>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
+        
     );
 };
 
